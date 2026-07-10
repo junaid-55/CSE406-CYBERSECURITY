281ece2c931708d0dd522f8035b07e17c06b6a45
junaid-55 <saikat12340@gmail.com>
2026-07-10 23:36:35 +0600

    off1 shesh

--- OFFLINE1/2105006_aes.py
+++ OFFLINE1/2105006_aes.py
@@ -1,13 +1,17 @@
 from BitVector import BitVector
 from aes_helpers import Sbox, InvSbox, Rcon, Mixer, InvMixer
 from typing import List, Optional, final
+import time
 
 
 class AES:
     def __init__(self, key, mode="ECB", key_size=128):
         self.org_key = key
         self.key = self.add_padding(key, key_size // 8, False)
+        t0 = time.perf_counter() * 1000
         self.expanded_key = self.key_expansion(BitVector(textstring=self.key))
+        t1 = time.perf_counter() * 1000
+        self.key_schedule_time = t1 - t0
         self.mode = mode
 
     def get_original_key(self):
@@ -15,6 +19,9 @@ class AES:
 
     def get_padded_key(self):
         return self.key
+
+    def get_key_chedule_time_ms(self):
+        return self.key_schedule_time
 
     def add_padding(self, text, block_size=16, is_text=True):
         if not is_text:
@@ -216,7 +223,11 @@ if __name__ == "__main__":
     print(f"In HEX(After padding: {repr(hex_string(aes.add_padding(str)))}")
 
     print("\nCiphered text:")
+
+    t0 = time.perf_counter() * 1000
     cipher = aes.encrypt_message(str)
+    t1 = time.perf_counter() * 1000
+
     ascii_str = bytes.fromhex(cipher).decode("latin-1")
     cipher_hex = hex_string(ascii_str)
     if mode == "CBC":
@@ -226,10 +237,19 @@ if __name__ == "__main__":
 
     print("\nDeciphered text:")
     print("Before Unpadding:")
+
+    t2 = time.perf_counter() * 1000
     final_text_padded = aes.decrypt_message(cipher, True)
+    t3 = time.perf_counter() * 1000
+
     print(f"In ASCII: {final_text_padded}")
     print(f"In HEX: {hex_string(final_text_padded)}")
     print("After Unpadding:")
     final_text = aes.decrypt_message(cipher)
     print(f"In ASCII: {final_text}")
     print(f"In HEX: {hex_string(final_text)}")
+
+    print("\nExecution time details:")
+    print(f"Key Schedule Time: {aes.get_key_chedule_time_ms()} ms")
+    print(f"Encryption Time  : {t1 - t0} ms")
+    print(f"Decryption Time  :{t3 - t2} ms")
